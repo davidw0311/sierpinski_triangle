914bff74b95ed477a695643124bf4dea1a8f0465
david <david311@student.ubc.ca>
2022-12-23 01:25:33 -0800

    refactored code

--- sierpinski.py
+++ sierpinski.py
@@ -1,28 +1,25 @@
 import cv2 
 import numpy as np
 import random
-
 from PIL import Image
 
-    
 XMAX = 1000
 YMAX = 1000
-BORDER = 100
+BORDER = 50
 
 corners = {1:(XMAX//2, 0), 2:(0,YMAX), 3:(XMAX,YMAX)}
 
 def getMidPoint(x1,y1,x2,y2):
     return ((x1+x2)//2, (y1+y2)//2)
 
-img = np.zeros((XMAX,YMAX))
-
 x = XMAX//2
 y = YMAX//2
 
 limg = np.zeros((XMAX+2*BORDER, YMAX+2*BORDER))
 
+# get frames
 frames = []
-for i in range(10000):
+for i in range(20000):
     (cx,cy) = random.choice(list(corners.values()))
     (x,y) = getMidPoint(x,y,cx,cy)
     limg[BORDER+y, BORDER+x]=255 
@@ -33,7 +30,7 @@ for i in range(10000):
         
 cv2.imwrite("sierpinski.jpg",limg)
 
-
+# create gif
 frame_one = frames[0]
 print("saving gif ...")
 frame_one.save("sierpinski.gif", format="GIF", append_images=frames,
